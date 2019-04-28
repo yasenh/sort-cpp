8dbe3bffbd3ab9f89719d40eb1e0b150523e9a4d
yasenh <yasenhu789@gmail.com>
2019-04-27 22:30:53 -0400

    add run docker script

--- docker_run.sh
+++ docker_run.sh
@@ -1,24 +1,21 @@
-DOCKER_NAME=jjanzic/docker-python3-opencv
+DOCKER_NAME=sort
 
 XSOCK=/tmp/.X11-unix
 XAUTH=/home/$USER/.Xauthority
-SHARED_DIR=/root/sort-cpp
-HOST_DIR=/home/$USER/sort-cpp
 
-mkdir -p $HOST_DIR
-echo "Shared directory: ${HOST_DIR}"
+xhost +
 
 docker run \
     -it --rm \
-    --volume=$HOST_DIR:$SHARED_DIR:rw \
-    --volume=$XSOCK:$XSOCK:rw \
-    --volume=$XAUTH:$XAUTH:rw \
     --env="XAUTHORITY=${XAUTH}" \
     --env="DISPLAY=${DISPLAY}" \
     --env="QT_X11_NO_MITSHM=1" \
     --privileged \
     -v /dev/bus/usb:/dev/bus/usb \
     -v /dev/video0:/dev/video0 \
-    --net=host \
-    $DOCKER_NAME \
-    /bin/bash
+    -v $XSOCK:$XSOCK:rw \
+    -v $XAUTH:$XAUTH:rw \
+    -v /home/$USER:/home/$USER \
+    -u $(id -u ${USER}):$(id -g ${USER}) \
+    --name sort-cpp \
+    $DOCKER_NAME /bin/bash
